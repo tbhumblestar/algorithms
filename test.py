70174e2a42b08648d58e204630cb364cdee79c81
김영빈 <colock123@gmail.com>
2022-09-17 23:30:23 +0900

    test

--- test.py
+++ test.py
@@ -10,129 +10,58 @@
 - 몸이 길어진 만큼, 그전의 행동을 추적해서 오면안되는 곳에 추가. 새로운 곳으로 이동할 경우, 맨 뒤의 값은 제거하고 새로 이동된값을 가면 안되는 곳으로 넣어줌 > 큐
 """
 
+#문제요약
+"""
+-n의 수가 크지 않음 > 완전탐색
+-조건이 없다면, build_frame의 입력값에 따라 result를 그려주는 건 어렵지 않다
+-그러면 build_frame이 요구하는 케이스별로, 실행해도 되는지 여부를 체크해주면 됨
+-build_Frame이 요구하는 건 결국 2*2임. [설치or제거] * [바닥or보]
+"""
+
+
 n = int(input())
-k = int(input())
+field = [[0]*n for _ in range(n)]
 
-A_place_list = []
+#for local test build_frame input
+import json
+build_frame = json.loads(input())
 
-for i in range(k):
-    x,y = map(int,input().split())
-    A_place_list.append((x,y))
+result = []
 
-move_list = []
-move_count = int(input())
-time = 0
-for i in range(move_count):
+#작업진행여부 체크함수
+def build_checker(frame,result):
     
-    x, c = input().split()
-
-    x = int(x) - time
-    time += x
+    #기둥 삭제
+    if frame[2] == 0 and frame[3] == 0:
+        pass
     
-    move_list.append((int(x),c))
-
-#마지막값 추가(이게 있어야 마지막 회전 후에도 꾸준히 움직임)
-move_list.append((10000,'R'))
-
-#이차원 리스트 생성 + 외곽벽=1로 지정
-field_list = [[0]*(n+2) for _ in range(n+2)]
-for i in range(n+2):
-    for j in range(n+2):
-        if i == 0 or i == n+1:
-            field_list[i][j] = 1
-        
-        if j == 0 or j == n+1:
-            field_list[i][j] = 1
-
-
-
-for place in A_place_list:
-    field_list[place[0]][place[1]] = "A"
-
-
-#뱀 위치. x가 행임!!!!
-x,y = 1,1
-#directioin
-direction = 'e'
-#direction_dict
-
-L_dict = {
-    'e':'n',
-    'n':'w',
-    'w':'s',
-    's':'w'
-}
-#direction_dict
-R_dict = {
-    'e':'s',
-    's':'w',
-    'w':'n',
-    'n':'e'
-}
-#
-dx = {
-    'e':0,
-    'w':0,
-    's':+1,
-    'n':-1,
-}
-
-dy = {
-    'e':1,
-    'w':-1,
-    's':0,
-    'n':0,
-}
-
-#뱀 몸 길이, 이전 위치
-prev_place = [(1,1)]
-
-#움직인 시간
-play_count = 0
-
-fin = False
-
-
-for move in move_list:
-    if fin:
-        #마지막 츄라이를 더해줘야 함
-        break
+    #기둥 설치
+    if frame[2] == 0 and frame[3] == 1:
+        pass
     
-    for i in range(0,move[0]):
+    #보 삭제
+    if frame[2] == 1 and frame[3] == 0:
+        pass
+    
+    #보 설치
+    if frame[2] == 1 and frame[3] == 1:
+        pass
+
+
+#작업시작
+for frame in build_frame:
+    
+    #작업을 진행해도 되는 경우
+    if build_checker(frame,result):
         
-        #방향에 따른 좌표변환
-        x += dx[direction]
-        y += dy[direction]
+        #삭제
+        if frame[3] == 0:
+            result = [i for i in result if i[0] != frame[0] and i[1] != frame[1]]
         
-        
-        
-        if field_list[x][y] == 1 or (x,y) in prev_place:
-            fin = True
-            break
-        
-        #사과일경우 몸 길이 증가 > 가면안되는 장소 추가
-        if field_list[x][y] == 'A':
-            prev_place.append((x,y))
-            #사과제거
-            field_list[x][y] = 0
+        #설치
+        if frame[3] == 1:
+            result.append(frame[0:2])
             
-        else:
-            del prev_place[0]
-            prev_place.append((x,y))
         
-        #방향 전환
-        if i == move[0]-1:
-            if move[1] == 'L':
-                direction = L_dict[direction]
-            
-            else:
-                direction = R_dict[direction]
-                
-        print(x,y)
-        print(prev_place)
-        play_count += 1
-
-print(x,y)
-#마지막 값을 더해줘야 함
-play_count += 1
-print(play_count)
+        
+        
--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
-move_list = []
-move_count = int(input())
-for i in range(move_count):
-    x, c = input().split()
-    
-    move_list.append((int(x),c))
+result = [[1,2],[3,4],[5,6]]
+frame = [1,2]
+
+
+result = [i for i in result if i[0] != frame[0] and i[1] != frame[1]]
+
+print(result)
