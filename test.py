86a17047bd4a9d9abaae46cec47b6820f030b000
김영빈 <colock123@gmail.com>
2022-09-23 20:45:40 +0900

    이코테/ch5 Q3

--- test.py
+++ test.py
@@ -2,62 +2,51 @@
 
 #풀이
 """
-- N,w의 개수가 적음 > 딱봐도 완전탐색
-- 원의 특성상, 원의 끝쪽과 원의 시작점을 이어줄 수도 있음
-    + 계산이 편하도록 원을 총 길이 2배가 되는 직선으로 만들어줘서, 원의 끝쪽과 원의 시작점쪽도 동일한 방식으로 계산할 수 있게 해줌
-- 직원들을 배치할 순서를 순열을 돌려줘야 함
-    + 처음에는 가장 큰 dist를 가진애들만 배치해주면 되니까 순열돌릴필요 없지 않나? 라고 생각했는데, 순열에 따라 값이 다름..ㅅㅂ..
-- 각 직원의 마지막 검사 지점과 그 다음으로 나오는 weak point를 비교해서, weak point가 더 클 경우, 해당 weakpoint에서 직원이 검사를 시작하는 방식
-- 직원을 더 추가할 수 없는데, weakpoint가 남아있으면 실패
+- 주변에 0이 있을 경우, 1로 바꾸는 재귀함수를 만듬
+- 값들이 전부 1로 변할때까지 직접적으로 실행해야 하는 재귀함수의 횟수를 카운트
 """
 
-from itertools import permutations
+n,m = map(int,input().split())
+field = []
+for i in range(n):
+    field.append(list(map(int,input())))
 
-def solution(n,weak,dist):
+#시작
+def dfs(x,y) -> None:
     
-    #weak 개수
-    weak_length = len(weak)
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
     
-    #원을 펼치기
-    for i in range(weak_length):
-        weak.append(weak[i]+n)
     
-    dist_perms = permutations(dist,len(dist))
     
-    min_count = len(dist)+1
-    
-    for dist in dist_perms:
-        #검사 시작
-        for start_weak_index in range(weak_length):
-            count = 1
-            
-            #이거 -1 안해줘도 됨!!! 4에서 시작해서 2칸을 가면 4,5,6을 체크하는 것임!!
-            last_check_place = weak[start_weak_index] + dist[count-1]
+    field[x][y] = 1
+    dfs(x,y-1)
+    dfs(x,y+1)
+    dfs(x-1,y)
+    dfs(x+1,y)
+    return False
 
-            for i in range(start_weak_index,start_weak_index+weak_length):
-                
-                if weak[i] > last_check_place:
-                    count +=1
-                    if count > len(dist):
-                        break
-                    last_check_place = weak[i] + dist[count -1]
-                
-            min_count = min(min_count,count)
-    
-    if min_count > len(dist):
-        return -1
-    
-    return min_count
 
-#testcase1
-# n = 12
-# weak = [1,5,6,10]
-# dist = [1,2,3,4]
+count = 0
 
-# #testcase2
-# n = 12
-# weak = [1,3,4,9,10]
-# dist = [3,5,7]
+for x in range(n):
+    for y in range(m):
+        if field[x][y] == 0:
+            print(x,y)
+            count += 1
+            dfs(x,y)
 
-#for test
-# print(solution(n,weak,dist))
+print(count)
+
+#testcase
+# 4 5
+# 00110
+# 00011
+# 11111
+# 00000
+
+#testcase2
+# 3 3
+# 001
+# 010
+# 101
--- 이것이 코딩 테스트다!/DFS/BFS(CH5,CH13)/FG_ch5_실전문제3_음료 얼려 먹기.py
+++ 이것이 코딩 테스트다!/DFS/BFS(CH5,CH13)/FG_ch5_실전문제3_음료 얼려 먹기.py
@@ -6,6 +6,8 @@
 - 값들이 전부 1로 변할때까지 직접적으로 실행해야 하는 재귀함수의 횟수를 카운트
 """
 
+
+
 n,m = map(int,input().split())
 field = []
 for i in range(n):
@@ -51,3 +53,43 @@ print(count)
 # 001
 # 010
 # 101
+
+#실패한 풀이
+"""
+-전반적으로 동일했는데, field[x][y]==0 체크하는 게 dfs함수 밖에 있음
+> dfs함수안에서 dfs가 조건없이 무한정 계속됨
+> 원래는 좌표값으로 한정시켜주면 될거라 생각했지만 좌표값안에서 무한히 증식함
+> 예를 들어 n이 3이면 x값이 1>2>1>2>1>2>1>2>1 이런식으로 무한히 증식될 수 있음
+"""
+
+n,m = map(int,input().split())
+field = []
+for i in range(n):
+    field.append(list(map(int,input())))
+
+#시작
+def dfs(x,y) -> None:
+    
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    
+    
+    
+    field[x][y] = 1
+    dfs(x,y-1)
+    dfs(x,y+1)
+    dfs(x-1,y)
+    dfs(x+1,y)
+    return False
+
+
+count = 0
+
+for x in range(n):
+    for y in range(m):
+        if field[x][y] == 0:
+            print(x,y)
+            count += 1
+            dfs(x,y)
+
+print(count)
