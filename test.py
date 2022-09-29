908f9baa2563bcbe4389070e9044425d264922f9
김영빈 <colock123@gmail.com>
2022-09-30 03:23:46 +0900

    이코테/ch13 Q18

--- test.py
+++ test.py
@@ -1,4 +1,49 @@
-let = 'abcdefg'
+def dfs(letters):
+    
+    answer = ''
+    if letters == '':
+        return letters
+    
+    left_count = 0
+    right_count = 0
+    
+    for i,v in enumerate(letters):
+        if v == '(':
+            left_count += 1
+        else:
+            right_count += 1
+            
+        if left_count == right_count:
+            
+            u = letters[:i+1]
+            v = letters[i+1:]
+            
+            if u[-1] == ')':
+                answer = u + dfs(v)
+            else:
+                new_u = ''
+                for i in u[1:-1]:
+                    if i == '(':
+                        new_u += ')'
+                    else:
+                        new_u += '('
+                
+                answer += '(' + dfs(v) + ')' + new_u
+        
+            return answer
+        
 
-for i,v in enumerate(let):
-    print(i,v)
+def solution(p):
+    
+    answer = dfs(p)
+    return answer
+
+#test1
+p ='(()())()'
+
+#test2
+p = ')('
+
+#test3
+p ='()))((()'
+print(solution(p))
