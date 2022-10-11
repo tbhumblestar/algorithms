251e106567189b2ecb08a8cb053810571a6fc222
김영빈 <colock123@gmail.com>
2022-10-11 21:37:18 +0900

    이코테/ch14 Q25

--- test2.py
+++ test2.py
@@ -1,22 +1,3 @@
-import heapq
-
-n = int(input())
-
-heap = []
-#힙에 자료 구조가 다 들어감
-for i in range(n):
-    data = int(input())
-    heapq.heappush(heap,data)
-    
-res = 0
-#힙에 원소가 1개일 떄가지
-while len(heap) != 1:
-    one = heapq.heappop(heap)
-    two = heapq.heappop(heap)
-    
-    sum_value = one + two
-    res += sum_value
-    heapq.heappush(heap,sum_value)
-    
-print(res)
-    
+print( 0 /2)
+cnt = [0 for i in range(5)]
+print(cnt)
