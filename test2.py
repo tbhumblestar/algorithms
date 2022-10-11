6427e18c60ea9a036e084099b3ca5ec4f1c7197c
김영빈 <colock123@gmail.com>
2022-10-11 19:14:33 +0900

    이코테/ch14 Q26

--- test2.py
+++ test2.py
@@ -1,3 +1,22 @@
-A = [1,2,3,4,3,3,3,5]
-A.sort()
-print(A)
+import heapq
+
+n = int(input())
+
+heap = []
+#힙에 자료 구조가 다 들어감
+for i in range(n):
+    data = int(input())
+    heapq.heappush(heap,data)
+    
+res = 0
+#힙에 원소가 1개일 떄가지
+while len(heap) != 1:
+    one = heapq.heappop(heap)
+    two = heapq.heappop(heap)
+    
+    sum_value = one + two
+    res += sum_value
+    heapq.heappush(heap,sum_value)
+    
+print(res)
+    
