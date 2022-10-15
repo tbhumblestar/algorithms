1bceac511878efba22e826254f630cad4c76051b
김영빈 <colock123@gmail.com>
2022-10-15 21:15:51 +0900

    이코테/ch16_Q33_퇴사

--- test2.py
+++ test2.py
@@ -1,24 +1,24 @@
-
-n = int(input())
-dp = [] # 다이나믹 프로그래밍을 위한 DP 테이블 초기화
+n = int(input()) # 전체 상담 개수
+t = [] # 각 상담을 완료하는데 걸리는 기간
+p = [] # 각 상담을 완료했을 때 받을 수 있는 금액
+dp = [0] * (n + 1) # 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
+max_value = 0
 
 for _ in range(n):
-    dp.append(list(map(int, input().split())))
+    x, y = map(int, input().split())
+    t.append(x)
+    p.append(y)
 
-# 다이나믹 프로그래밍으로 2번째 줄부터 내려가면서 확인
-for i in range(1, n):
-    for j in range(i + 1):
-        # 왼쪽 위에서 내려오는 경우
-        if j == 0:
-            up_left = 0
-        else:
-            up_left = dp[i - 1][j - 1]
-        # 바로 위에서 내려오는 경우
-        if j == i:
-            up = 0
-        else:
-            up = dp[i - 1][j]
-        # 최대 합을 저장
-        dp[i][j] = dp[i][j] + max(up_left, up)
+# 리스트를 뒤에서부터 거꾸로 확인
+for i in range(n - 1, -1, -1):
+    time = t[i] + i
+    # 상담이 기간 안에 끝나는 경우
+    if time <= n:
+        # 점화식에 맞게, 현재까지의 최고 이익 계산
+        dp[i] = max(p[i] + dp[time], max_value)
+        max_value = dp[i]
+    # 상담이 기간을 벗어나는 경우
+    else:
+        dp[i] = max_value
 
-print(max(dp[n - 1]))
+print(max_value)
