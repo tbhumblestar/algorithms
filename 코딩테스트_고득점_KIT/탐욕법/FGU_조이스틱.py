a001d124938660802759351d092fd38578a89e73
김영빈 <colock123@gmail.com>
2022-10-20 23:53:52 +0900

    고득점_Kit/탐욕법/조이스틱 해답 추가

--- 코딩테스트_고득점_KIT/탐욕법/FGU_조이스틱.py
+++ 코딩테스트_고득점_KIT/탐욕법/FGU_조이스틱.py
@@ -1,6 +1,9 @@
 #문제종류 : 그리디
 #링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42860
 
+#유용한 부분 : 프로그래머스 1등 풀이.. 
+#프로그래머스
+
 #풀이
 """
 -필요한 건 두가지.
@@ -12,7 +15,7 @@
     일단 최대값을 len(name)-1로 둠
     A를 군집화시킴. A에는 갈필요가 없으므로, 모든 A의 군집의 왼쪽에서 시작하는 것과 오른쪽에서 시작하는 것을 전부 계산해줌
     
-
+#프로그래머스 풀이에 프로그래머스 풀이에 대한 설명있음
 
 """
 
@@ -105,3 +108,43 @@ name = 'AABBBABAAAABBAABAAAAAAZBBAA'
 
 print("len:",len(name))
 print(solution(name))
+
+
+
+### 프로그래머스 풀이
+
+def solution(name):
+    answer = 0
+    n = len(name)
+
+    #자리에서 다른 글자로 바꾸기 위한 카운트 계산 함수
+    def alphabet_to_num(char):
+        num_char = [i for i in range(14)] + [j for j in range(12, 0, -1)]
+        return num_char[ord(char) - ord('A')]
+
+    #글자 카운트
+    for ch in name:
+        answer += alphabet_to_num(ch)
+
+    move = n - 1
+    
+    #이 부분이 핵심
+    #모든 인덱스마다 완전탐색처럼 이동해야 하는 횟수를 구함
+    #단 이때의 인덱스는 시작점이라기 보다는 A의 cluster가 있는지 확인하는 느낌임
+    for idx in range(n):
+        
+        
+        next_idx = idx + 1
+        
+        #A의 클러스터가 있는지, 있다면 어디까지인지를 선택
+        while (next_idx < n) and (name[next_idx] == 'A'):
+            next_idx += 1
+        
+        #정방향과 역방향중 가까운 방향을 선택
+        distance = min(idx, n - next_idx)
+        
+        #min의 두번재 인자는 클러스터를 기준으로 왼쪽한번 오른쪽한번, 그리고 가까운쪽한번을 더한 것임 ㄸ
+        move = min(move, idx + (n - next_idx) + distance)
+
+    answer += move
+    return answer
