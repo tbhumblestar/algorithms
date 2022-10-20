be5d69178d017f01ca2309cb328051f5a276b8b1
김영빈 <colock123@gmail.com>
2022-10-20 12:12:10 +0900

    고득점_Kit/완전탐색/카펫

--- test2.py
+++ test2.py
@@ -1,15 +1,1 @@
-a = "123"
-
-from itertools import permutations
-
-a = list(permutations(a,2))
-print(a)
-
-a = ('1','2')
-b = ''.join(a)
-print(b)
-
-print(int('12'))
-print(int('012'))
-
-print(int('12') == int('012'))
+print(''.join(['ㅁ','ㅠ']))
