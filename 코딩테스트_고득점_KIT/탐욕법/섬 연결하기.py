0830d4b20c8bc22e63196c496afef720f151f2e7
김영빈 <colock123@gmail.com>
2022-10-24 22:53:01 +0900

    고득점_Kit/탐욕법/섬 연결하기

---
+++ 코딩테스트_고득점_KIT/탐욕법/섬 연결하기.py
@@ -0,0 +1,21 @@
+
+#문제종류 : 그리디
+#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42861
+#풀이
+"""
+#풀이
+
+"""
+
+def solution(n, costs):
+    answer = 0
+    return answer
+
+
+#t1
+n = 4
+costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
+res =4 
+
+
+print(solution(n, costs))
