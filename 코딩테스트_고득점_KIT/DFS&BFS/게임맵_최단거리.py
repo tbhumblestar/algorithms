33e2b87fc0e64bb34028144bd7ff11825368bc38
김영빈 <colock123@gmail.com>
2022-11-01 20:01:58 +0900

    고득득점_Kit/DFS&BFS/게임맵최단거리

---
+++ 코딩테스트_고득점_KIT/DFS&BFS/게임맵_최단거리.py
@@ -0,0 +1,20 @@
+#문제종류 : dfs/bfs
+#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/1844
+#풀이
+"""
+
+"""
+def solution(maps):
+    answer = 0
+    return answer
+
+
+#t1
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
+ans = 11
+
+#t2
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+ans = -1
+
+print(solution(maps))
