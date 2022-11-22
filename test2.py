d825575c3def179082d8b5079f4442ca7c846805
김영빈 <colock123@gmail.com>
2022-11-23 02:32:28 +0900

    고득점_Kit/Dynamic_programming/N으로 표현

--- test2.py
+++ test2.py
@@ -1,10 +1,1 @@
-from collections import defaultdict 
-
-default_dict1 = defaultdict(list)
-default_dict2 = defaultdict(set)
-default_dict3 = defaultdict(str)
-default_dict4 = defaultdict(int)
-print(default_dict1['1'])
-print(default_dict2['2'])
-print(default_dict3['3'])
-print(default_dict4['4'])
+print(4 // -2)
