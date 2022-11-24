adfc018e9c592e10e94e5527c8dc81edc770f9d0
김영빈 <colock123@gmail.com>
2022-11-24 16:03:24 +0900

    고득점_Kit/Dynamic_programming/등굣길

--- test1.py
+++ test1.py
@@ -1,80 +1,110 @@
 #문제종류 : 다이나믹프로그래밍
-#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42895
+#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42898
 
 #풀이
 """
--완전탐색이되, dp를 사용해 경우의 수를 줄인 것임
--수들의 조합은 다음과 같음 : 55, 5+5 , 5*5, 5/5, 5-5
--또한 수의 개수 조합을 전부 계산해줘야 함
-    예를 들어, 수가 4개이다 > 수가 1개 & 3개, 2개 & 2개, 3개 & 1개의 케이스를 모두 계산해줘야 함
--사칙연산이 아니라 그냥 숫자를 이어붙이는 경우(55,555,5555...)등에 대해서는 따로 처리해줘야 함
+-주의 : 문제가 m,n을 바꿔서 줌(문제오류). 따라서 코드를 제출할 때, solution함수의 m,n을 바꿔서 줘야 정답이 맞음
+-오른쪽과 아래로만 이동함
+-따라서 특정칸은 바로 윗칸과 바로 왼쪽칸에 오는 방법을 합한 것임
+-매칸을 저장하는 다이나믹프로그래밍과 BFS를 합쳐서 풀면됨
+-BFS는 visitied가 필요함
 """
+from collections import deque
 
+#동,남,서,북
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
-def lst_appender(a,b,next_lst):
-    next_lst.append(a+b)
-    next_lst.append(a*b)
-    next_lst.append(a-b)
-    next_lst.append(b-a)
-    
-    if b != 0:
-        next_lst.append(a//b)
-    if a != 0:
-        next_lst.append(b//a)
-    
+#m,n바꿔줘야 하고(문제오류)
+# x,y 둘 중 하나라도 2,2일때 , 1,2가 웅덩이이면 값이 2로 나와야 하는데 1번으로 나옴(테케1)
+#효율성테스트전부아작남. > 방문체크를 위한 필드가 필요함
 
-def solution(N, number):
+def solution(m, n, puddles):
+    dp = [[0]*m for i in range(n)]
+    visited = [[0]*m for i in range(n)]
+    dp[0][0] = 1
     
-    if N == number:
-        return 1
-    
-    d = []
-    d.append([N])
-    
-    def next_lst_maker(d,N,curr_idx):
-        for i in range(curr_idx):
-            other_i = curr_idx - (i+1)
-            
-            for j in d[i]:
-                for q in d[other_i]:
-                    lst_appender(j,q,d[curr_idx])
-    
-    #idx : 1~7
-    for idx in range(1,8):
-        d.append([])
-        next_lst_maker(d,N,idx)
+    for x,y in puddles:
+        dp[x-1][y-1] = -1
 
-        special_num = 0
-        for i in range(0,idx+1):
-            special_num += N*(10**(i))
+    queue = deque([])
+    # print(dp)
+    if m >=2:
+        if dp[1][0] == 0:
+            queue.append((1,0))
+    
+    if n >=2:
+        if dp[0][1] == 0:
+            queue.append((0,1))
+    
+    while queue:
+        # print("now_dp : ",dp)
+        # print("now_queue:",queue)
         
-        # print(idx,special_num)
+        
+        x,y = queue.popleft()
+        
+        cnt = 0
+        for i in range(2,4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            #벽이라면 pass
+            if nx < 0 or ny < 0:
+                continue
+            
+            #웅덩이라면 pass
+            if dp[nx][ny] == -1:
+                continue
+            
+            cnt += dp[nx][ny]
+            
+        # print(dp)
+        dp[x][y] = cnt
+        
+        #왼쪽과 위 모두 웅덩이인 경우
+        if cnt == -2:
+            cnt = 0
+            
+        dp[x][y] = cnt
+        
+        
+        #동사1
+        
+        for i in range(2):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            #벽 밖으로 나간다면 pass
+            if nx < 0 or nx >= n or ny <0 or ny >= m:
+                continue
+            
+            #웅덩이라면 pass
 
-        d[idx].append(special_num)
-        d[idx] = list(set(d[idx]))
+            if dp[nx][ny] != 0:
+                continue
+            
+            if visited[nx][ny] != 0:
+                continue
+            
+            visited[nx][ny] = 1
+            queue.append((nx,ny))
+    # print(dp)
+    return (dp[n-1][m-1] % 1000000007)
 
-        if number in d[idx]:
-            return idx+1
+#t1
+m = 4
+n = 3
+# puddles	= [[2, 2]]
 
-    # for i in d:
-    #     print()
-    #     print(i)
-    #     print()
-    return -1
-
-# t1
-N = 5
-number = 12
-# res = 4
-
-# # t2
-# N = 2
-# number = 150
-# # res = 3
+#t2
+m = 4
+n = 3
+puddles = []
 
 #t3
-N = 8
-number = 5800
+# m = 4
+# n = 4
+puddles = [[1,2]]
 
-print("시작!")
-print(solution(N,number))
+print(solution(m,n,puddles))
